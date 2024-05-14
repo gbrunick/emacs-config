0db1de4affbc9fd2e51e0031080ad54b34bbebe6
Gerard Brunick <gbrunick@gmail.com>
2024-05-13 23:22:10 -0400

    Improve gpb-packages.el

--- gpb-packages.el
+++ gpb-packages.el
@@ -1,6 +1,16 @@
 ;; gpb-packages.el
 ;;
-;; Tools to manage and install all files in a local package archive.
+;; Tools to install and manage packages in a local version controlled
+;; package archive.  The list of packages is recorded in the version
+;; controlled file external-packages/selected-packages.txt.
+;;
+;; Run `gpb-ensure-packages-installed' in your init file to ensure that
+;; exactly the packages listed in `selected-packages' are installed from
+;; the tar files in external-packages/.
+;;
+;; To upgrade or install new packages, use `list-packages' in the usual
+;; way, run `gpb-update-local-elpa' to update the local package archive and
+;; then commmit the changes to version control.
 ;;
 
 (require 'package)
@@ -10,11 +20,24 @@
 (setq gpb-local-elpa (expand-file-name "external-packages"
                                        (file-name-directory
                                         (or load-file-name (buffer-file-name))))
-      package-archives `(("gpb-local-elpa" . ,gpb-local-elpa))
-      gpb-selected-packages (list 'evil 'evil-collection 'evil-goggles
-                                  'ess 'poly-R
-                                  'wgrep
-                                  'elpa-mirror))
+      gpb-selected-packages-file (expand-file-name "selected-packages.txt"
+                                                   gpb-local-elpa)
+      gpb-selected-packages (read (with-temp-buffer
+                                    (insert-file-contents
+                                     gpb-selected-packages-file)
+                                    (buffer-string))))
+
+
+(defun gpb-ensure-packages-installed ()
+  "Ensure that all packages in the local package archive are installed."
+  ;; If have any missing packages, we install them all.
+  (let ((package-archives `(("gpb-local-elpa" . ,gpb-local-elpa)))
+        (uninstalled-packages (cl-remove-if #'package-installed-p
+                                            gpb-selected-packages)))
+    (when uninstalled-packages
+      (package-refresh-contents)
+      (dolist (pkg gpb-selected-packages)
+        (package-install pkg)))))
 
 
 (defun gpb-update-local-elpa (&optional arg)
@@ -22,28 +45,10 @@
 
 With a prefix argument, we recreate the mirror directory."
   (interactive "P")
-  (unless (package-installed-p 'elpa-mirror)
-    (let ((package-archives `(("gpb-local-elpa" . ,gpb-local-elpa))))
-      (package-refresh-contents)
-      (package-install "elpa-mirror")))
-  (require 'elpa-mirror)
-  (elpamr-create-mirror-for-installed gpb-local-elpa arg))
+  (elpamr-create-mirror-for-installed gpb-local-elpa arg)
+  (with-temp-buffer
+    (insert (pp-to-string gpb-selected-packages))
+    (write-file gpb-selected-packages-file)))
 
-
-(defun gpb-manage-packages ()
-  "Use ELPA and MELPA to install new packages"
-  (interactive)
-  (setq package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
-                           ("melpa" . "https://melpa.org/packages/")))
-  (package-refresh-contents)
-  (list-packages))
-
-
-(defun gpb-ensure-packages-installed ()
-  "Ensure that all packages in the local package archive are installed."
-  (dolist (pkg gpb-selected-packages)
-    (unless (package-installed-p pkg)
-      (package-install pkg t)))
-  (package--save-selected-packages gpb-selected-packages))
 
 (provide 'gpb-packages)
