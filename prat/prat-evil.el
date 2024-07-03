2eebde6c5b54e97982a5b33e4abdd6c6c7ac3c87
Gerard Brunick <gbrunick@gmail.com>
2024-07-02 22:32:00 -0400

    Improve prat's evil keybindings

--- prat/prat-evil.el
+++ prat/prat-evil.el
@@ -1,13 +1,66 @@
 ;; Don't cover the prat buffer bindings.  There aren't that many.
 (with-eval-after-load 'evil
-  (evil-make-overriding-map prat-command-output-mode-map)
-  (evil-make-overriding-map prat-commit-graph-mode-map)
-  (evil-make-overriding-map prat-edit-mode-map)
-  (evil-make-overriding-map prat-hunk-selection-mode-map)
-  (evil-make-overriding-map prat-hunk-view-mode-map)
-  (evil-make-overriding-map prat-show-commit-files-mode-map)
-  (evil-make-overriding-map prat-show-status-mode-map)
-  (evil-make-overriding-map prat-staged-changes-mode-map)
-  (evil-make-overriding-map prat-unstaged-changes-mode-map))
+  ;; (evil-make-overriding-map prat-command-output-mode-map)
+  ;; (evil-make-overriding-map prat-commit-graph-mode-map)
+  ;; (evil-make-overriding-map prat-edit-mode-map)
+  ;; (evil-make-overriding-map prat-hunk-selection-mode-map)
+  ;; (evil-make-overriding-map prat-hunk-view-mode-map)
+  ;; (evil-make-overriding-map prat-show-commit-files-mode-map)
+  ;; (evil-make-overriding-map prat-show-status-mode-map)
+  ;; (evil-make-overriding-map prat-staged-changes-mode-map)
+  ;; (evil-make-overriding-map prat-unstaged-changes-mode-map))
+
+  (evil-define-key 'normal prat-command-output-mode-map
+    "!" 'prat-shell-command
+    "q" 'quit-window)
+
+  (evil-define-key 'normal prat-commit-graph-mode-map
+    "m" 'prat-mark-line
+    "d" 'prat-commit-graph-mode--show-diff
+    "gr" 'prat-refresh-buffer
+    (kbd "RET") 'prat-show-commit-graph--show-commit
+    "!" 'prat-shell-command)
+
+  (evil-define-key 'normal prat-hunk-selection-mode-map
+    "\t" 'prat-forward-command
+    [(backtab)] 'prat-backward-command
+    "p" 'prat-backward-command
+    "P" 'prat-backward-file-command
+    "n" 'prat-forward-command
+    "N" 'prat-forward-file-command
+    (kbd "RET") 'prat-goto-line
+    "gr" 'revert-buffer)
+
+  (evil-define-key 'normal prat-hunk-view-mode-map
+    "\t" 'prat-forward-command
+    [(backtab)] 'prat-backward-command
+    "p" 'prat-backward-command
+    "P" 'prat-backward-file-command
+    "n" 'prat-forward-command
+    "N" 'prat-forward-file-command
+    (kbd "RET") 'prat-goto-line
+    "gr" 'revert-buffer)
+
+  (evil-define-key 'normal prat-show-commit-files-mode-map
+    (kbd "RET") 'prat-show-commit-graph--show-file-version)
+
+  (evil-define-key 'normal prat-show-status-mode-map
+    "\t" 'forward-button
+    "gr" 'prat-show-status--refresh
+    "m" 'prat-show-status--mark-file
+    "u" 'prat-show-status--unmark-file
+    "a" 'prat-show-status--stage-files
+    "r" 'prat-show-status--unstage-files
+    "d" 'prat-show-status--show-diff
+    "!" 'prat-shell-command)
+
+  (evil-define-key 'normal prat-staged-changes-mode-map
+    "r" 'prat-unstage-hunks)
+
+  (evil-define-key 'normal prat-unstaged-changes-mode-map
+    "a" 'prat-stage-hunks
+    "d" 'prat-revert-marked-hunks
+    "w" 'prat-toggle-whitespace-diff-args))
+
 
 (provide 'prat-evil)
